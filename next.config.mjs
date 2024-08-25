a26ebbcca90f673226b85f344a6874d42bd2dcb3
Shams Misbah <mailshamsmisbah@gmail.com>
2024-08-26 01:05:46 +0530

    pushing the final version

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol: 'http',
+                hostname: 'res.cloudinary.com',
+                port:''
+            }
+        ]
+    }
+};
+
+export default nextConfig;
